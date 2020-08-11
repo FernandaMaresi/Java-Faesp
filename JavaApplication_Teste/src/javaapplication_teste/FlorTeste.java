c632dada0b57b1dd9699715b6baa83c6ad4374ec
Fernanda Maresi <ferferborges@gmail.com>
2020-08-11 11:02:46 -0300

    Create FlorTeste.java

---
+++ JavaApplication_Teste/src/javaapplication_teste/FlorTeste.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication_teste;
+
+/**
+ *
+ * @author ferfe
+ */
+public class FlorTeste {
+   
+    public double [] Features;
+    public String Name;
+    public FlorTeste(){}
+   
+    //comprimento sepala, largura da sepala, comprimento da sepala , largura da sepala, nome
+    FlorTeste(String name, double sepalLength, double sepalWidth, double petalLength, double petalWidth){   
+        Features = new double[4];
+        this.Name = name;
+        Features[0] = sepalLength;
+        Features[1] = sepalWidth;
+        Features[2] = petalLength;
+        Features[3] = petalWidth;
+    }
+}
